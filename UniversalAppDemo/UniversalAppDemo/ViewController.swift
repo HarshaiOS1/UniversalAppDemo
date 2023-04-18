acf2f5a57eda1ffd3dcdb517a38e2ff533d05ef0
HarshaiOS1 <kharsha21@yahoo.in>
2023-04-18 20:58:40 +0200

    Initial commit

---
+++ UniversalAppDemo/UniversalAppDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniversalAppDemo
+//
+//  Created by Harsha on 18/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
