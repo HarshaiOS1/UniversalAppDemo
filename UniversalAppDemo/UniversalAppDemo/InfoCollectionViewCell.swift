95ed6b0e92a83293bc543fa4b0acdf333d21d645
HarshaiOS1 <kharsha21@yahoo.in>
2023-04-21 19:48:39 +0200

    added ui for collecion view layout

---
+++ UniversalAppDemo/UniversalAppDemo/InfoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  InfoCollectionViewCell.swift
+//  UniversalAppDemo
+//
+//  Created by Harsha on 21/04/2023.
+//
+
+
+import UIKit
+
+class InfoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+}
